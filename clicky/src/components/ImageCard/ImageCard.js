3b2ebadd9d7103b1dec30d4b55a21384a5fbd812
michelle <michellemchoe@gmail.com>
2019-02-01 22:21:56 -0800

    trying to add onclick

--- clicky/src/components/ImageCard/ImageCard.js
+++ clicky/src/components/ImageCard/ImageCard.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./ImageCard.css";
 
-const ImageCard = props => (
-    <div className="game-image">
-        <div class="card">
-            <div class="card-image">
-                <figure class="image is-128x128">
-                    <img src={props.image} alt="orange" />
-                </figure>
+
+
+
+const ImageCard = props => {
+    const Clicked = () => {
+        if (props.click === true) {
+            console.log("Already true");
+        }
+    }
+
+    return (
+        <div className="game-image" onClick={Clicked()}>
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-128x128">
+                        <img src={props.image} alt="orange" />
+                    </figure>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+}
 export default ImageCard;
